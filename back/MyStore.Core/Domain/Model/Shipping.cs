21313e8ea97cc0ed46b87ac53d8056b54f36fc53
outtayerloop <wiemchouchane@yahoo.fr>
2022-05-02 18:25:14 +0200

    Added shipping unit tests

--- back/MyStore.Core/Domain/Model/Shipping.cs
+++ back/MyStore.Core/Domain/Model/Shipping.cs
@@ -4,8 +4,17 @@ namespace MyStore.Core.Domain.Model
 {
     public class Shipping : BaseModel
     {
-        public ShippingPackage Package { get; }
+        public ShippingPackage Package { get; set; }
 
-        public decimal Price { get; }
+        public decimal Price { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Shipping shipping)
+                return false;
+            return shipping.Id == Id
+                && shipping.Package == Package
+                && shipping.Price == Price;
+        }
     }
 }
